022ba2da73da890fe1d5b7227ce46f9ac3628c12
Felipe Espinoza <felipe.espinoza@schibsted.com>
2019-11-05 09:49:58 +0100

    Initial Commit

---
+++ HelloXcodeExtensions/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloXcodeExtensions
+//
+//  Created by Felipe Espinoza on 05/11/2019.
+//  Copyright © 2019 Felipe Espinoza. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HelloXcodeExtensions/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloXcodeExtensions
+//
+//  Created by Felipe Espinoza on 05/11/2019.
+//  Copyright © 2019 Felipe Espinoza. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
